246af5279efa089daa134ff01627ed341c80c7fa
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-02-13 11:18:38 +0000

    Added needed tableview methods also, created toolbar for naviagation controller.

--- shopping-list/ViewController.swift
+++ shopping-list/ViewController.swift
@@ -9,14 +9,37 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    private var items = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Shopping List"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        
+        toolbarItems = [spacer, addButton]
+          
+        navigationController?.isToolbarHidden = false
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    @objc private func addItem() {
+        
+    }
 
 }
 
